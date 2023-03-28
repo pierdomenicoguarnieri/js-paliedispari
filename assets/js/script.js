c166ebabb957193d3598501646a179eba7edf133
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-28 17:58:44 +0200

    add:palindrome check

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,8 +5,19 @@ result = wordCheck(word);
 function wordCheck(word){
   //Acquisisco il punto centrale della parola per poterlo saltare nel controllo della parola palindroma con lunghezza dispari
   let wordMiddlePoint;
+
+  let flag = true;
+
   if(word.length % 2){
     wordMiddlePoint = Math.floor((word.length)/2);
   }
+
+  for(let i = 0; i < word.length; i++){
+    if(i === wordMiddlePoint) continue;
+
+    if(word[i] !== word[word.length - 1]) flag = false;
+  }
+
+  return flag;
 }
 
