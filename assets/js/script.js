879b8e6f22c11431bf31c14be14af2154aed192b
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-28 17:59:02 +0200

    add: output

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 const word = prompt("Inserisci una parola");
 
+const output = document.getElementById("output");
+
 result = wordCheck(word);
+
+output.innerHTML = result ? "Hai inserito la parola " + word + " ed è palindroma" : "Hai inserito la parola " + word + " e non è palindroma.";
 
 function wordCheck(word){
   //Acquisisco il punto centrale della parola per poterlo saltare nel controllo della parola palindroma con lunghezza dispari
