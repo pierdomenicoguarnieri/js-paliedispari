fa1269c437a062cbe48bcca5fda1675badf0c9d6
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-28 19:08:13 +0200

    add: function calculateWinner

--- js-pari-e-dispari/assets/js/script.js
+++ js-pari-e-dispari/assets/js/script.js
@@ -12,7 +12,32 @@ do {
 
 const nComputer = randomNumber();
 
+const message = calculateWinner(choice, n, nComputer);
+
 function randomNumber(){
   let n = Math.floor(Math.random() * 5) + 1;
   return n;
+}
+
+function calculateWinner(scelta, nUtente, nComputer){
+  let message
+
+  const sum = nUtente + nComputer;
+
+  let flag = (sum % 2) ? false : true;
+
+  switch (scelta){
+    case "pari":
+      message = flag ? "Hai vinto!" : "Hai perso.";
+      break;
+
+    case "dispari":
+      message = flag ? "Hai perso." : "Hai vinto!";
+      break;
+
+    default:
+      message = "Errore!"
+  }
+
+  return message;
 }
