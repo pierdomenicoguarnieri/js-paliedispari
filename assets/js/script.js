6dcf4dbea919bb5b25ae0e9874788ce0735f7a10
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-28 17:57:55 +0200

    add: middle point calc

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,5 +3,10 @@ const word = prompt("Inserisci una parola");
 result = wordCheck(word);
 
 function wordCheck(word){
+  //Acquisisco il punto centrale della parola per poterlo saltare nel controllo della parola palindroma con lunghezza dispari
+  let wordMiddlePoint;
+  if(word.length % 2){
+    wordMiddlePoint = Math.floor((word.length)/2);
+  }
 }
 
