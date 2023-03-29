c0c368eb32301b78005877700d07dab4f523687c
pierdomenicoguarnieri <pierdo.guarnieri@gmail.com>
2023-03-29 09:51:44 +0200

    fix: wrong inex in for cycle

--- js-palindroma/assets/js/script.js
+++ js-palindroma/assets/js/script.js
@@ -17,7 +17,7 @@ function wordCheck(word){
   for(let i = 0; i < word.length; i++){
     if(i === wordMiddlePoint){
       continue;
-    }else if(word[i] !== word[word.length - 1]){
+    }else if(word[i] !== word[(word.length - 1) - i]){
       flag = false;
     }
   }
